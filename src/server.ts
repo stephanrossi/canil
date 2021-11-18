b1f93f57dcf0b61315e322684756f83abeabe7e7
unknown <stephanrossi@gmail.com>
2021-11-18 20:45:15 -0300

    final

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import dotenv from "dotenv"
 import mustache from "mustache-express"
 import path from "path"
 
-import MainRoutes from "./routes"
-import { Server } from "http"
 
 //Initialize DOTENV
 dotenv.config()
@@ -21,7 +19,6 @@ app.engine("mustache", mustache())
 app.use(express.static(path.join(__dirname, "../public")))
 
 //Routes
-app.use(MainRoutes)
 
 //404
 app.use((req, res) => {
