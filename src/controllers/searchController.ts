7dd933e7867128b7b1827015d4b63afb930ba801
unknown <stephanrossi@gmail.com>
2022-03-15 21:51:24 -0300

    Terminado

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -5,6 +5,11 @@ import { createMenuObject } from "../helpers/createMenuObject"
 export const search: RequestHandler = (req, res) => {
   let query: string = req.query.q as string
 
+  if (!query) {
+    res.redirect('/')
+    return
+  }
+
   let list = Pet.getFromName(query)
 
   res.render("pages/page", {
